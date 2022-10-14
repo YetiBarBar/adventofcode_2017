6050639bd01bf7c7770f6c5f626de20464782322
YetiBarBar <yetibarbar@gmail.com>
2022-10-14 21:09:57 +0200

    Add solution for day 5

---
+++ src/day5.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let input = include_str!("../data/day5.data");
+    let mut input_data: Vec<isize> = input
+        .lines()
+        .map(str::trim)
+        .map(str::parse)
+        .map(Result::unwrap)
+        .collect();
+
+    let mut count = 0;
+    let mut cur_pos: isize = 0;
+    while let Some(value) = input_data.get_mut(cur_pos as usize) {
+        count += 1;
+        cur_pos += *value;
+        *value += 1;
+        // println!("{:?}", &input);
+    }
+    println!("Part 1: {}", count);
+    let mut input_data: Vec<isize> = input
+        .lines()
+        .map(str::trim)
+        .map(str::parse)
+        .map(Result::unwrap)
+        .collect();
+
+    let mut count = 0;
+    let mut cur_pos: isize = 0;
+    while let Some(value) = input_data.get_mut(cur_pos as usize) {
+        count += 1;
+        cur_pos += *value;
+        *value += if *value >= 3 { -1 } else { 1 };
+        // println!("{:?}", &input);
+    }
+    println!("Part 2: {}", count);
+}
